47f5020ac61f0e2cc7bf715482e5a48232469f17
gourojushiva <gourojushiva@gmail.com>
2018-02-16 22:27:35 +0530

    ffrr

---
+++ app/src/main/java/com/something/laundryapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.something.laundryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
