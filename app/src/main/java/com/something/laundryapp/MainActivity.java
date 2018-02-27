0dfc3867efee8df0085a613744c345a7d8c0d193
gourojushiva <shivagourju@gmail.com>
2018-02-27 20:34:59 +0530

    Pushed

--- app/src/main/java/com/something/laundryapp/MainActivity.java
+++ app/src/main/java/com/something/laundryapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hellooo  hiii ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Helllloooo hiiiiii ", Toast.LENGTH_SHORT).show();
     }
 }
